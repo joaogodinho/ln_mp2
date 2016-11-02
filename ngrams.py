6dd83136f9b8e0bbd41568ef38f909c02aa28aee
joaogodinho <joaocfgodinho@gmail.com>
2016-11-02 22:59:42 +0000

    Version 1, ready for delivery.

--- ngrams.py
+++ ngrams.py
@@ -20,7 +20,7 @@ AUTHORS = [
         'JoseSaramago',
         'LuisaMarquesSilva'
 ]
-TOKEN_REGEX = r'\b\w+-?\w*\b'
+TOKEN_REGEX = r'\b\w+\'?\w*-?\w*\b'
 
 
 
@@ -75,7 +75,7 @@ if __name__ == '__main__':
             temp = temp.replace('_', '')
             train += [temp]
     # Generate the (uni/bi)grams files for evaluation
-    # generate_grams(train)
+    generate_grams(train)
     test = []
     for i in range(6):
         with open(PATH2 + '/500Palavras/text{}.txt'.format(i+1), 'r', encoding='UTF-8') as file:
@@ -84,20 +84,192 @@ if __name__ == '__main__':
             temp = '\n'.join(list(filter(lambda x: x is not '', temp)))
             temp = temp.replace('_', '')
             test += [temp]
+    for i in range(6):
+        with open(PATH2 + '/1000Palavras/text{}.txt'.format(i+1), 'r', encoding='UTF-8') as file:
+            temp = file.read()
+            temp = temp.split('\n')
+            temp = '\n'.join(list(filter(lambda x: x is not '', temp)))
+            temp = temp.replace('_', '')
+            test += [temp]
 
-    cv = CountVectorizer(token_pattern=TOKEN_REGEX, ngram_range=(1, 1), max_features=100)
+    cv = CountVectorizer(token_pattern=TOKEN_REGEX, ngram_range=(1, 2))
     dtm = cv.fit_transform(train + test)
     dtm = dtm.toarray()
     dist = np.round(1 - cosine_similarity(dtm), 2)
 
     # Generate table header
-    header = ''
-    for auth in AUTHORS:
-        header += auth[:5] + '\t'
+    header = '\t'
+    for a in AUTHORS:
+        header += '{}\t'.format(a[:5])
     for i in range(6):
         header += 'text{}\t'.format(i+1)
+    for i in range(6):
+        header += 'text{}\t'.format(i+1)
+    # Generate table
+    table = ''
+    for idx, row in enumerate(dist):
+        if idx < 6:
+            table += '{}\t'.format(AUTHORS[idx][:5])
+        else:
+            table += 'text{}\t'.format((idx % 6) + 1)
+        lowest = min(row)
+        for item in row:
+            table += '{:.2f}\t'.format(item)
+        table += '\n'
 
     # Print table
+    print('(uni/bi)grams all texts:')
     print(header)
-    print('\n'.join([''.join(['{:.2f}\t'.format(item) for item in row]) for row in dist]))
+    print(table)
 
+
+    cv = CountVectorizer(token_pattern=TOKEN_REGEX, ngram_range=(1, 1))
+    dtm = cv.fit_transform(train + test)
+    dtm = dtm.toarray()
+    dist = np.round(1 - cosine_similarity(dtm), 2)
+
+    # Generate table header
+    header = '\t'
+    for i in range(6):
+        header += 'text{}\t'.format(i+1)
+    for i in range(6):
+        header += 'text{}\t'.format(i+1)
+    # Generate table
+    table = ''
+    for idx, row in enumerate(dist[:6]):
+        table += '{}\t'.format(AUTHORS[idx][:5])
+        for item in row[6:]:
+            table += '{:.2f}\t'.format(item)
+        table += '\n'
+
+    # Print table
+    print('Unigrams:')
+    print(header)
+    print(table)
+
+
+    cv = CountVectorizer(token_pattern=TOKEN_REGEX, ngram_range=(1, 2))
+    dtm = cv.fit_transform(train + test)
+    dtm = dtm.toarray()
+    dist = np.round(1 - cosine_similarity(dtm), 2)
+
+    # Generate table header
+    header = '\t'
+    for i in range(6):
+        header += 'text{}\t'.format(i+1)
+    for i in range(6):
+        header += 'text{}\t'.format(i+1)
+    # Generate table
+    table = ''
+    for idx, row in enumerate(dist[:6]):
+        table += '{}\t'.format(AUTHORS[idx][:5])
+        for item in row[6:]:
+            table += '{:.2f}\t'.format(item)
+        table += '\n'
+
+    # Print table
+    print('Bigrams:')
+    print(header)
+    print(table)
+
+
+    # cv = CountVectorizer(token_pattern=TOKEN_REGEX, ngram_range=(1, 1), lowercase=False)
+    # dtm = cv.fit_transform(train + test)
+    # dtm = dtm.toarray()
+    # dist = np.round(1 - cosine_similarity(dtm), 2)
+
+    # # Generate table header
+    # header = '\t'
+    # for i in range(6):
+    #     header += 'text{}\t'.format(i+1)
+    # for i in range(6):
+    #     header += 'text{}\t'.format(i+1)
+    # # Generate table
+    # table = ''
+    # for idx, row in enumerate(dist[:6]):
+    #     table += '{}\t'.format(AUTHORS[idx][:5])
+    #     for item in row[6:]:
+    #         table += '{:.2f}\t'.format(item)
+    #     table += '\n'
+
+    # # Print table
+    # print('Unigrams with lowercase:')
+    # print(header)
+    # print(table)
+
+
+    # cv = CountVectorizer(token_pattern=TOKEN_REGEX, ngram_range=(2, 2), lowercase=False)
+    # dtm = cv.fit_transform(train + test)
+    # dtm = dtm.toarray()
+    # dist = np.round(1 - cosine_similarity(dtm), 2)
+
+    # # Generate table header
+    # header = '\t'
+    # for i in range(6):
+    #     header += 'text{}\t'.format(i+1)
+    # for i in range(6):
+    #     header += 'text{}\t'.format(i+1)
+    # # Generate table
+    # table = ''
+    # for idx, row in enumerate(dist[:6]):
+    #     table += '{}\t'.format(AUTHORS[idx][:5])
+    #     for item in row[6:]:
+    #         table += '{:.2f}\t'.format(item)
+    #     table += '\n'
+
+    # # Print table
+    # print('Bigrams with lowercase:')
+    # print(header)
+    # print(table)
+
+
+    stop_words = ['a', 'as', 'com', 'como', 'da', 'de', 'do', 'e', 'em', 'no', 'não', 'o', 'os', 'para', 'por', 'que', 'se', 'um', 'uma', 'é']
+    cv = CountVectorizer(token_pattern=TOKEN_REGEX, ngram_range=(1, 1), stop_words=stop_words)
+    dtm = cv.fit_transform(train + test)
+    dtm = dtm.toarray()
+    dist = np.round(1 - cosine_similarity(dtm), 2)
+
+    # Generate table header
+    header = '\t'
+    for i in range(6):
+        header += 'text{}\t'.format(i+1)
+    for i in range(6):
+        header += 'text{}\t'.format(i+1)
+    # Generate table
+    table = ''
+    for idx, row in enumerate(dist[:6]):
+        table += '{}\t'.format(AUTHORS[idx][:5])
+        for item in row[6:]:
+            table += '{:.2f}\t'.format(item)
+        table += '\n'
+
+    # Print table
+    print('Unigrams with top 20 stop words ({}):'.format(stop_words))
+    print(header)
+    print(table)
+
+
+    # stop_words = ['a sua', 'com a', 'com o', 'como se', 'de que', 'de um', 'e a', 'e o', 'em que', 'não é', 'o que', 'o seu', 'os olhos', 'para a', 'para o', 'que a', 'que não', 'que o', 'que se', 'é que']
+    # cv = CountVectorizer(token_pattern=TOKEN_REGEX, ngram_range=(2, 2), stop_words=stop_words)
+    # dtm = cv.fit_transform(train + test)
+    # dtm = dtm.toarray()
+    # dist = np.round(1 - cosine_similarity(dtm), 2)
+
+    # # Generate table header
+    # header = '\t'
+    # for i in range(6):
+    #     header += 'text{}\t'.format(i+1)
+    # for i in range(6):
+    #     header += 'text{}\t'.format(i+1)
+    # # Generate table
+    # table = ''
+    # for idx, row in enumerate(dist[:6]):
+    #     table += '{}\t'.format(AUTHORS[idx][:5])
+    #     for item in row[6:]:
+    #         table += '{:.2f}\t'.format(item)
+    #     table += '\n'
+
+    # # Print table
+    # print('Bigrams with top 20 stop words ({}):'.format(stop_words))
+    # print(header)
+    # print(table)
--- run.sh
+++ run.sh
@@ -44,3 +44,4 @@ function normalize {
 
 init
 normalize
+python ngrams.py
